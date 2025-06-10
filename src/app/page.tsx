0082c894483445f4ef6256ec37de81694621ff0f
Hilooong <johnzittur@gmail.com>
2025-06-10 13:04:21 +0800

    01:setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <div className="text-3xl font-bold underline text-yellow-500">Hello World</div>;
+    return (
+        <div className="flex flex-row items-center justify-center h-screen gap-4">
+            <Button variant={"custom2"}>Click me</Button>
+            <Button variant={"custom"}>Click me</Button>
+        </div>
+    );
 }
